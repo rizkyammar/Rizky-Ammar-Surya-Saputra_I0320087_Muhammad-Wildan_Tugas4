526c996f7040bae31e2d478db7451eb3acb9600b
rizkyammar <80144906+rizkyammar@users.noreply.github.com>
2021-03-26 15:42:11 +0700

    Update I0320087_Soal 6_Tugas 4.py

--- I0320087_Soal 6_Tugas 4.py
+++ I0320087_Soal 6_Tugas 4.py
@@ -3,22 +3,27 @@ print("        KELUARAN DARI OPERATOR BITWISE")
 print("""
 ==================================================""")
 
-a = 4
-b = 11
+a = 4   #4 = 0000 0100
+b = 11  #11 = 0000 1011
 
-c = a | b;
+#4 | 11
+c = a | b   #15 = 0000 1111
 print("Hasil dari 4|11 adalah :", c)
 
-c = a >> b
+#4 >> 11
+c = a >> b  #0 = 0000 0000
 print("Hasil dari 4>>11 adalah :", c)
 
-c = a ^ b
+#4 ^ 11
+c = a ^ b   #15 = 0000 1111
 print("Hasil dari 4 ^ 11 adalah :", c)
 
-c = ~a
+#~4
+c = ~a      #-5 = 0000 0101
 print("Hasil dari ~4 adalah :", c)
 
-c = b & a
+#11 & 4
+c = b & a   #0 = 0000 0000
 print("Hasil dari 11&4 adalah :", c)
 
 print("""   TERIMA KASIH TELAH MAMPIR
